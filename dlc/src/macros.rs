594d97f2f66f985fd4400f0b61a1370ca44519da
Polybius93 <adam.daniel.puskas@polybius-studios.com>
2023-09-06 15:15:59 +0200

    feat: added documentation to macros

--- dlc/src/macros.rs
+++ dlc/src/macros.rs
@@ -1,3 +1,6 @@
+/// This macro logs a message to the browser console using the `web_sys::console::log_1` function.
+/// It takes any number of arguments and formats them into a string using the `format!` macro.
+/// The resulting string is then passed to `web_sys::console::log_1` as an argument.
 #[macro_export]
 macro_rules! clog {
     ( $( $t:tt )* ) => {
@@ -5,6 +8,12 @@ macro_rules! clog {
     }
 }
 
+/// This macro is a wrapper around the `clog!` and `println!` macros.
+/// It logs a message to the console using `clog!` if the target architecture is `wasm32`,
+/// or prints the message to the standard output using `println!` otherwise.
+/// It takes any number of arguments and formats them into a string using the `format!` macro.
+/// The resulting string is then passed to either `clog!` or `println!` as an argument,
+/// depending on the target architecture.
 #[macro_export]
 macro_rules! log_to_console {
     ( $( $t:tt )* ) => {
