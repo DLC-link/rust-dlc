bb57fa03452d5ec96610ecc81def4fe9afc9b178
Polybius93 <adam.daniel.puskas@polybius-studios.com>
2023-09-06 14:49:28 +0200

    feat: simplfied logic

--- dlc/src/lib.rs
+++ dlc/src/lib.rs
@@ -275,84 +275,76 @@ impl PartyParams {
         fee_rate_per_vb: u64,
         extra_fee: u64,
     ) -> Result<(TxOut, u64, u64), Error> {
-        let (change_output, fund_fee, cet_or_refund_fee) = match self.collateral {
-            0 => (
-                TxOut {
-                    value: self.input_amount,
-                    script_pubkey: self.change_script_pubkey.clone(),
-                },
-                0,
-                0,
-            ),
-            _ => {
-                let mut inputs_weight: usize = 0;
+        let mut inputs_weight: usize = 0;
 
-                for w in &self.inputs {
-                    let script_weight = util::redeem_script_to_script_sig(&w.redeem_script)
-                        .len()
-                        .checked_mul(4)
-                        .ok_or(Error::InvalidArgument(format!("[get_change_output_and_fees] error: failed to transform a redeem script for a p2sh-p2w* output to a script signature")))?;
-                    inputs_weight = checked_add!(
-                        inputs_weight,
-                        TX_INPUT_BASE_WEIGHT,
-                        script_weight,
-                        w.max_witness_len
-                    )?;
-                }
+        for w in &self.inputs {
+            let script_weight = util::redeem_script_to_script_sig(&w.redeem_script)
+                .len()
+                .checked_mul(4)
+                .ok_or(Error::InvalidArgument(format!("[get_change_output_and_fees] error: failed to transform a redeem script for a p2sh-p2w* output to a script signature")))?;
+            inputs_weight = checked_add!(
+                inputs_weight,
+                TX_INPUT_BASE_WEIGHT,
+                script_weight,
+                w.max_witness_len
+            )?;
+        }
 
-                // Value size + script length var_int + ouput script pubkey size
-                let change_size = self.change_script_pubkey.len();
-                // Change size is scaled by 4 from vBytes to weight units
-                let change_weight =
-                    change_size
-                        .checked_mul(4)
-                        .ok_or(Error::InvalidArgument(format!(
-                            "[get_change_output_and_fees] error: failed to calculate change weight"
-                        )))?;
+        // Value size + script length var_int + ouput script pubkey size
+        let change_size = self.change_script_pubkey.len();
+        // Change size is scaled by 4 from vBytes to weight units
+        let change_weight = change_size
+            .checked_mul(4)
+            .ok_or(Error::InvalidArgument(format!(
+                "[get_change_output_and_fees] error: failed to calculate change weight"
+            )))?;
 
-                // Base weight (nLocktime, nVersion, ...) is distributed among parties
-                let this_party_fund_base_weight = FUND_TX_BASE_WEIGHT;
+        // Base weight (nLocktime, nVersion, ...) is distributed among parties
+        // independently of inputs contributed
+        let this_party_fund_base_weight = FUND_TX_BASE_WEIGHT;
 
-                let total_fund_weight = checked_add!(
-                    this_party_fund_base_weight,
-                    inputs_weight,
-                    change_weight,
-                    36
-                )?;
-                let fund_fee = util::weight_to_fee(total_fund_weight, fee_rate_per_vb)?;
+        let total_fund_weight = checked_add!(
+            this_party_fund_base_weight,
+            inputs_weight,
+            change_weight,
+            36
+        )?;
+        let fund_fee = util::weight_to_fee(total_fund_weight, fee_rate_per_vb)?;
 
-                // Base weight (nLocktime, nVersion, funding input ...) is distributed
-                // among parties independently of output types
-                let this_party_cet_base_weight = CET_BASE_WEIGHT;
+        // Base weight (nLocktime, nVersion, funding input ...) is distributed
+        // among parties independently of output types
+        let this_party_cet_base_weight = CET_BASE_WEIGHT;
 
-                // size of the payout script pubkey scaled by 4 from vBytes to weight units
-                let output_spk_weight =
-                    self.payout_script_pubkey
-                        .len()
-                        .checked_mul(4)
-                        .ok_or(Error::InvalidArgument(format!(
-                    "[get_change_output_and_fees] error: failed to calculate payout script pubkey weight"
-                )))?;
-                let total_cet_weight = checked_add!(this_party_cet_base_weight, output_spk_weight)?;
-                let cet_or_refund_fee = util::weight_to_fee(total_cet_weight, fee_rate_per_vb)?;
-
-                let required_input_funds =
-                    checked_add!(self.collateral, fund_fee, cet_or_refund_fee, extra_fee)?;
-
-                if self.input_amount < required_input_funds {
-                    return Err(Error::InvalidArgument(format!("[get_change_output_and_fees] error: input amount is lower than the sum of the collateral plus the required fees => input_amount: {}, collateral: {}, fund fee: {}, cet_or_refund_fee: {}, extra_fee: {}", self.input_amount, self.collateral, fund_fee, cet_or_refund_fee, extra_fee)));
-                }
-
-                let change_output = TxOut {
-                    value: self.input_amount - required_input_funds,
-                    script_pubkey: self.change_script_pubkey.clone(),
-                };
-
-                (change_output, fund_fee, cet_or_refund_fee)
-            }
+        // size of the payout script pubkey scaled by 4 from vBytes to weight units
+        let output_spk_weight =
+            self.payout_script_pubkey
+                .len()
+                .checked_mul(4)
+                .ok_or(Error::InvalidArgument(format!(
+            "[get_change_output_and_fees] error: failed to calculate payout script pubkey weight"
+        )))?;
+        let total_cet_weight = checked_add!(this_party_cet_base_weight, output_spk_weight)?;
+        let cet_or_refund_fee = util::weight_to_fee(total_cet_weight, fee_rate_per_vb)?;
+        let required_input_funds: u64 = if self.collateral == 0 {
+            0
+        } else {
+            checked_add!(self.collateral, fund_fee, cet_or_refund_fee, extra_fee)?
         };
 
-        Ok((change_output, fund_fee, cet_or_refund_fee))
+        if self.input_amount < required_input_funds {
+            return Err(Error::InvalidArgument(format!("[get_change_output_and_fees] error: input amount is lower than the sum of the collateral plus the required fees => input_amount: {}, collateral: {}, fund fee: {}, cet_or_refund_fee: {}, extra_fee: {}", self.input_amount, self.collateral, fund_fee, cet_or_refund_fee, extra_fee)));
+        }
+
+        let change_output = TxOut {
+            value: self.input_amount - required_input_funds,
+            script_pubkey: self.change_script_pubkey.clone(),
+        };
+
+        if fee_rate_per_vb == 0 {
+            Ok((change_output, 0, 0))
+        } else {
+            Ok((change_output, fund_fee, cet_or_refund_fee))
+        }
     }
 
     fn get_unsigned_tx_inputs_and_serial_ids(&self, sequence: Sequence) -> (Vec<TxIn>, Vec<u64>) {
@@ -428,7 +420,7 @@ pub(crate) fn create_fund_transaction_with_fees(
     let total_collateral = checked_add!(offer_params.collateral, accept_params.collateral)?;
 
     let (offer_change_output, offer_fund_fee, offer_cet_fee) =
-        offer_params.get_change_output_and_fees(fee_rate_per_vb, extra_fee)?;
+        offer_params.get_change_output_and_fees(0, extra_fee)?;
     let (accept_change_output, accept_fund_fee, accept_cet_fee) =
         accept_params.get_change_output_and_fees(fee_rate_per_vb, extra_fee)?;
 
@@ -438,6 +430,8 @@ pub(crate) fn create_fund_transaction_with_fees(
         - offer_fund_fee
         - accept_fund_fee
         - extra_fee;
+
+    log_message("fund_output_value:", fund_output_value);
 
     assert_eq!(
         total_collateral + offer_cet_fee + accept_cet_fee + extra_fee,
