539ce6752bc66ff8b8a4c6866c681a49bf28556e
Polybius93 <adam.daniel.puskas@polybius-studios.com>
2023-09-18 16:12:33 +0200

    feat: changed get_change_output calls

--- dlc/src/lib.rs
+++ dlc/src/lib.rs
@@ -419,8 +419,13 @@ pub(crate) fn create_fund_transaction_with_fees(
 ) -> Result<(Transaction, Script), Error> {
     let total_collateral = checked_add!(offer_params.collateral, accept_params.collateral)?;
 
-    let (offer_change_output, offer_fund_fee, offer_cet_fee) =
-        offer_params.get_change_output_and_fees(0, extra_fee)?;
+    let offer_change_output = TxOut {
+        value: 0,
+        script_pubkey: offer_params.change_script_pubkey.clone(),
+    };
+    let offer_fund_fee = 0;
+    let offer_cet_fee = 0;
+
     let (accept_change_output, accept_fund_fee, accept_cet_fee) =
         accept_params.get_change_output_and_fees(fee_rate_per_vb, extra_fee)?;
 
